f110d408aab489cc4c491d4ae5929828c8b0b4b3
Dominik Zogg <dominik.zogg@gmail.com>
2013-02-21 19:52:29 +0100

    work with the contao extra block

--- src/ContaoCommunityAlliance/ComposerInstaller/ModuleInstaller.php
+++ src/ContaoCommunityAlliance/ComposerInstaller/ModuleInstaller.php
@@ -9,7 +9,21 @@ class ModuleInstaller extends LibraryInstaller
 {
     public function getInstallPath(PackageInterface $package)
     {
-        return '../system/modules/' . $package->getPrettyName();
+        $extra = $package->getExtra();
+
+        if(!array_key_exists('contao', $extra))
+        {
+            throw new \ErrorException("A contao-module needs the contao declaration within the extra block!");
+        }
+
+        $contao = $extra['contao'];
+
+        if(!array_key_exists('target', $contao))
+        {
+            throw new \ErrorException("Please add a target key to the contao section, _composer for example (folder name in contao)!");
+        }
+
+        return '../system/modules/' . $contao['target'];
     }
 
     /**
