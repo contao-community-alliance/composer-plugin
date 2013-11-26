e6ce5682931ec307e1d025847584cfb3f31109fd
Tristan Lins <tristan.lins@bit3.de>
2013-11-26 20:34:00 +0100

    Make the RunonceExecutor multi-usablewq.

--- src/ContaoCommunityAlliance/ComposerInstaller/RunonceExecutorTemplate.php
+++ src/ContaoCommunityAlliance/ComposerInstaller/RunonceExecutorTemplate.php
@@ -4,37 +4,37 @@ namespace ContaoCommunityAlliance\ComposerInstaller;
 
 use System;
 
-class RunonceExecutor extends System
-{
-	public function __construct()
+if (!class_exists('ContaoCommunityAlliance\ComposerInstaller\RunonceExecutor')) {
+	class RunonceExecutor extends System
 	{
-		parent::__construct();
-	}
+		public function __construct()
+		{
+			parent::__construct();
+		}
 
-	public function run()
-	{
-		$runonces = TEMPLATE_RUNONCE_ARRAY;
-
-		foreach ($runonces as $runonce) {
-			try {
-				require_once(TL_ROOT . DIRECTORY_SEPARATOR . $runonce);
-			}
-			catch (\Exception $e) {
-				// first trigger an error to write this into the log file
-				trigger_error(
-					$e->getMessage() . "\n" . $e->getTraceAsString(),
-					E_USER_ERROR
-				);
-				// now log into the system log
-				$this->log(
-					$e->getMessage() . "\n" . $e->getTraceAsString(),
-					'RunonceExecutor run()',
-					'ERROR'
-				);
+		public function run($runonces)
+		{
+			foreach ($runonces as $runonce) {
+				try {
+					require_once(TL_ROOT . DIRECTORY_SEPARATOR . $runonce);
+				}
+				catch (\Exception $e) {
+					// first trigger an error to write this into the log file
+					trigger_error(
+						$e->getMessage() . "\n" . $e->getTraceAsString(),
+						E_USER_ERROR
+					);
+					// now log into the system log
+					$this->log(
+						$e->getMessage() . "\n" . $e->getTraceAsString(),
+						'RunonceExecutor run()',
+						'ERROR'
+					);
+				}
 			}
 		}
 	}
 }
 
 $executor = new RunonceExecutor();
-$executor->run();
+$executor->run(TEMPLATE_RUNONCE_ARRAY);
