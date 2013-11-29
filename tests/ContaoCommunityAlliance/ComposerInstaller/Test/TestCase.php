f992292b79759e6bcc53a70fcdf3d7d16063c4c9
Christian Schiffler <c.schiffler@cyberspectrum.de>
2013-11-29 06:05:07 +0100

    Add abstract base TestCase class to populate later on.

---
+++ tests/ContaoCommunityAlliance/ComposerInstaller/Test/TestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Contao Composer Installer
+ *
+ * Copyright (C) 2013 Contao Community Alliance
+ *
+ * @package contao-composer
+ * @author  Dominik Zogg <dominik.zogg@gmail.com>
+ * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @link    http://c-c-a.org
+ * @license LGPL-3.0+
+ */
+
+namespace ContaoCommunityAlliance\ComposerInstaller\Test;
+
+abstract class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
