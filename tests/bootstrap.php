2240bd84c88f06127e6a25d4286cdf0920456edb
Tristan Lins <tristan.lins@bit3.de>
2013-11-28 21:47:48 -0500

    Introduce unit tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Contao Composer Installer
+ *
+ * Copyright (C) 2013 Contao Community Alliance
+ *
+ * @package contao-composer
+ * @author  Dominik Zogg <dominik.zogg@gmail.com>
+ * @author  Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author  Tristan Lins <tristan.lins@bit3.de>
+ * @link    http://c-c-a.org
+ * @license LGPL-3.0+
+ */
+
+error_reporting(E_ALL);
+
+function includeIfExists($file)
+{
+	return file_exists($file) ? include $file : false;
+}
+
+if ((!$loader = includeIfExists(__DIR__.'/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__.'/../../../autoload.php'))) {
+	echo 'You must set up the project dependencies, run the following commands:'.PHP_EOL.
+		'curl -sS https://getcomposer.org/installer | php'.PHP_EOL.
+		'php composer.phar install'.PHP_EOL;
+	exit(1);
+}
+
+$loader->add('ContaoCommunityAlliance\ComposerInstaller', __DIR__);
+
+require __DIR__.'/ContaoCommunityAlliance/ComposerInstaller/Test/TestCase.php';
