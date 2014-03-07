ac03a3036e22eb0f9e2af05cd99d10f8e4408498
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-03-07 17:34:34 +0100

    Make unit testing happy again.

--- tests/ContaoCommunityAlliance/Composer/Plugin/Test/TestCase.php
+++ tests/ContaoCommunityAlliance/Composer/Plugin/Test/TestCase.php
@@ -19,13 +19,22 @@ use Composer\Util\Filesystem;
 
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
-
 	protected function ensureDirectoryExistsAndClear($directory)
 	{
+		// Necessary to really clear the cache on travis-ci.
+		// Otherwise the info is outdated causing is_dir() to report true.
+		// This happened to confuse the tests of the symlink installer in all php versions from 5.3, 5.4, 5.5 but
+		// only on travis-ci. The development machines of @tristanlins and @discordier ran all tests just fine
+		// making this a PITA to debug. Thanks to the fine guys over at travis-ci who provided me with a debug machine,
+		// I got this issue sorted out. For discussion, refer to:
+		// https://github.com/contao-community-alliance/composer-plugin/issues/7
+		clearstatcache(true);
+
 		$fs = new Filesystem();
-		if (is_dir($directory)) {
-			$fs->removeDirectory($directory);
+		if (!$fs->removeDirectory($directory)) {
+			throw new \RuntimeException('Deletion of '.$directory.' was not successful.');
 		}
-		mkdir($directory, 0777, true);
+
+		$fs->ensureDirectoryExists($directory);
 	}
 }
